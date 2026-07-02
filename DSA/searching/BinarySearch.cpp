ecbb413be971c21db8743432f8370a73389812e2
Vivek sahu <vickysv1610@gmail.com>
2026-07-02 23:01:34 +0530

    Binary search using vector

--- DSA/searching/BinarySearch.cpp
+++ DSA/searching/BinarySearch.cpp
@@ -1,40 +1,85 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
 
 
-    int search;
-    cin>>search;
+//     int search;
+//     cin>>search;
 
-    int start = 0;
-    int end = n - 1;
-    bool found = false;
-    while(start <= end){
-        int mid = (start + end) / 2;
+//     int start = 0;
+//     int end = n - 1;
+//     bool found = false;
+//     while(start <= end){
+//         int mid = (start + end) / 2;
 
-        if( arr[mid] == search){
-            cout<<mid;
-            found  = true;
-            break;
+//         if( arr[mid] == search){
+//             cout<<mid;
+//             found  = true;
+//             break;
         
-        }
-        else if(arr[mid] < search){
-            start = mid + 1;
-        }
-        else{
-            end = mid - 1;
-        }
-    }
+//         }
+//         else if(arr[mid] < search){
+//             start = mid + 1;
+//         }
+//         else{
+//             end = mid - 1;
+//         }
+//     }
 
-    if(!found){
-        cout<< -1;
-    }
+//     if(!found){
+//         cout<< -1;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+  public:
+    bool binarySearch(vector<int>& arr, int k) {
+        // code here
+        
+        int s = 0;
+        int e = arr.size() - 1;
+        
+        while( s <= e){
+            int mid = (s + e) / 2;
+            
+            
+            if(arr[mid] == k){
+                return true;
+            }
+            else if(arr[mid] < k){
+                s = mid + 1;
+            }
+            else{
+                e = mid - 1;
+            }
+        }
+        return false;
+        
+    }
+};
