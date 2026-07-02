339041498a66522f937adc3a2705b91a974141a2
Vivek sahu <vickysv1610@gmail.com>
2026-07-02 22:38:48 +0530

    Largest of three number

---
+++ DSA/Basics/largestOfThreeNumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    if(a > b){
+        cout<<a;
+    }
+    else if(b > c){
+        cout<<b;
+    }
+    else{
+        cout<<c;
+    }
+}
