a25f35bd3748717f5bfc7df7f3f3386c5bc7ba23
Vivek sahu <vickysv1610@gmail.com>
2026-07-03 22:20:52 +0530

    Max element in the array

---
+++ DSA/pratices/maxElement.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+    int maxi = INT_MIN;
+    for(int i=0;i<n;i++){
+        if(arr[i] > maxi){
+            maxi = arr[i];
+        }
+
+
+    }
+    cout<<maxi;
+}
