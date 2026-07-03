5d3fdb50f22b315b156cabc9798408bedeec1407
Vivek sahu <vickysv1610@gmail.com>
2026-07-03 22:17:31 +0530

    binary search

--- DSA/searching/BinarySearch.cpp
+++ DSA/searching/BinarySearch.cpp
@@ -1,45 +1,43 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int n;
-//     cin>>n;
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
 
 
-//     int search;
-//     cin>>search;
+    int search;
+    cin>>search;
 
-//     int start = 0;
-//     int end = n - 1;
-//     bool found = false;
-//     while(start <= end){
-//         int mid = (start + end) / 2;
+    int start = 0;
+    int end = n - 1;
+    bool found = false;
+    while(start <= end){
+        int mid = (start + end) / 2;
 
-//         if( arr[mid] == search){
-//             cout<<mid;
-//             found  = true;
-//             break;
+        if( arr[mid] == search){
+            cout<<mid;
+            found  = true;
+            break;
         
-//         }
-//         else if(arr[mid] < search){
-//             start = mid + 1;
-//         }
-//         else{
-//             end = mid - 1;
-//         }
-//     }
+        }
+        else if(arr[mid] < search){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
 
-//     if(!found){
-//         cout<< -1;
-//     }
+    if(!found){
+        cout<< -1;
+    }
 
-//     return 0;
-// }
-
-
+    return 0;
+}
 
 
 
@@ -57,6 +55,8 @@
 
 
 
+
+/*
 class Solution {
   public:
     bool binarySearch(vector<int>& arr, int k) {
@@ -83,3 +83,5 @@ class Solution {
         
     }
 };
+
+*/
