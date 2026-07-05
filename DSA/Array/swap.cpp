a848b3bab0a7ffdcf3d3c91be8885c190bad720e
Vivek sahu <vickysv1610@gmail.com>
2026-07-05 11:24:05 +0530

    swap to element without using third element

--- DSA/Array/swap.cpp
+++ DSA/Array/swap.cpp
@@ -1,12 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    // int a,b;
+    // cin>>a>>b;
+
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    // cout<<a<<" "<<b;
+
+
+
+
+
     int a,b;
     cin>>a>>b;
 
-    int temp = a;
-    a = b;
-    b = temp;
-
+    a = a + b;
+    b = a - b;
+    a  = a - b;
     cout<<a<<" "<<b;
 }
